cd143f2db402aab92999daababb6225792bb6e4b
Leonardo Silva De Oliveira <Leonardo.Oliveira@caoa.com.br>
2019-07-16 19:21:33 -0300

    Alterando Codigo com o passo a passo do Renam

--- script.js
+++ script.js
@@ -1,57 +1,28 @@
 
+
 function embaralhar(lista){
-    let valor_temporario;
-    let indice_aleatorio; 
 
-    for(let i = lista.length -1; i !== 0; i--){
-        indice_aleatorio = Math.floor(Math.random() * i );
+    let valor_aleatorio;
+    let indice_aleatorio;
 
-        valor_temporario = lista[i];
+    for(let i = lista.length - 1; i !== 0; i --){
+        indice_aleatorio = Math.floor(Math.random() * i);
+
+        valor_aleatorio = lista[i];
         lista[i] = lista[indice_aleatorio];
-        lista[indice_aleatorio]= valor_temporario;
+        lista[indice_aleatorio] = valor_aleatorio;
     }
+
     return lista;
 }
 
-let cartas = document.querySelectorAll(".carta");
-
-let imagensSalvas = ["Barney_Stinson.jpg", "barney.jpg", "Lily_Aldrin.jpg", "Marshall_Eriksen.jpg", 
-"Robin_Scherbatsky.jpg", "Robin-2.jpg", "Ted_Mosby.jpg", "Ted-HQ-how-i-met.jpg"];
-
-let imagens = imagensSalvas.concat(imagensSalvas);
-
-let cartaVirada = null;
-
-imagens = embaralhar(imagens);
-
-for(let i = 0; i<cartas.length; i++){
-    cartas[i].style.backgroundImage = `url("../imagens/${imagens[i]}")`;
+function fechar(carta){
+    carta.style.backgroundImage = "url('imagens/Costas-das-cartas.jpg')"
+    carta.onClick = processarClique;
 }
 
 
-setTimeout(function(){
-    for(let carta of cartas){
-        carta.style.backgroundImage = 'url("imagens/Costas-das-cartas.jpg")';
-        carta.onClick = function(){
-
-            if(cartaVirada && cartaVirada.id !== carta.id){
-                setTimeout(function(){
-                    if(cartaVirada.style.backgroundImage === carta.style.backgroundImage){
-                        cartaVirada = null;
-                        cartaVirada.onClick = null;
-                        carta.onClick = null;
-                    }else{
-                        carta.style.backgroundImage = 'url("imagens/Costas-das-cartas.jpg")';
-                        cartaVirada.style.backgroundImage = 'url("imagens/Costas-das-cartas.jpg")';
-                    }
-                    cartaVirada = null;
-                }, 1500)
-                
-            }else{ 
-            carta.style.backgroundImage = `url("imagens/${imagens[Number(carta.id)]}")`;
-            cartaVirada = carta;
-            }
-        }
-    }
-
-}, 3000);
+function abrir(carta){
+    carta.style.backgroundImage = `url('imagens/${imagens[Number(carta.id)]}')`;
+    carta.onClick = null;
+}
